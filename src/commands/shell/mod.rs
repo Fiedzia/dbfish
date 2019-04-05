91cb2d6e0986fcee84fb789023e75b33f199e60e
Maciej Dziardziel <m.dziardziel@cv-library.co.uk>
2019-04-05 14:09:20 +0100

    add shell support for sqlite

--- src/commands/shell/mod.rs
+++ src/commands/shell/mod.rs
@@ -8,12 +8,26 @@ use crate::commands::common::{SourceConfigCommandWrapper, SourceConfigCommand};
 
 #[derive(StructOpt)]
 pub struct ShellCommand {
-    #[structopt(short = "c", long = "client", help = "select shell (client)", default_value="mysql")]
+    #[structopt(short = "c", long = "client", help = "select shell (client)", default_value="default")]
     pub client: String,
     #[structopt(subcommand)]
     pub source: SourceConfigCommandWrapper,
 }
 
+
+pub fn create_python_virtualenv(path: &std::path::PathBuf) {
+
+    if !path.exists() {
+        Command::new("python3")
+            .arg("-m")
+            .arg("virtualenv")
+            .arg("-p")
+            .arg("python3")
+            .arg(path.clone())
+            .status()
+            .expect("creation of virtualenv failed");
+    }
+}
 
 #[cfg(feature = "use_mysql")]
 pub fn mysql_client(mysql_config_options: &commands::common::MysqlConfigOptions) {
@@ -75,23 +89,17 @@ pub fn mysql_python_client(mysql_config_options: &commands::common::MysqlConfigO
     if !python_venv_dir.exists() {
         std::fs::create_dir(&python_venv_dir).unwrap();
     }
+
     let python_mysql_venv = python_venv_dir.join("mysql");
     if !python_mysql_venv.exists() {
-        Command::new("python3")
-            .arg("-m")
-            .arg("virtualenv")
-            .arg("-p")
-            .arg("python3")
-            .arg(python_mysql_venv.clone())
-            .status()
-            .expect("creation of virtualenv failed");
+        create_python_virtualenv(&python_mysql_venv);
         Command::new(python_mysql_venv.join("bin").join("pip"))
             .arg("install")
             .arg("ipython")
             .arg("pymysql")
             .status()
             .expect("could not install dependencies via pip");
-    };
+    }
     let python_file = python_mysql_venv.join("run.py");
     if !python_file.exists() {
         let content = include_str!("mysql.py");
@@ -107,17 +115,75 @@ pub fn mysql_python_client(mysql_config_options: &commands::common::MysqlConfigO
     if let Some(port) =  &mysql_config_options.port {
         std::env::set_var("MYSQL_PORT", port.to_string());
     }
-
     if let Some(password) = &mysql_config_options.password {
         std::env::set_var("MYSQL_PASSWORD", password);
     }
-
     if let Some(database) =  &mysql_config_options.database {
         std::env::set_var("MYSQL_DATABASE", database);
     }
 
-
     Command::new(python_mysql_venv.join("bin").join("python"))
+        .arg(python_file.clone())
+        .status()
+        .expect(&format!("could not run python script: {}", python_file.to_str().unwrap()));
+}
+
+
+#[cfg(feature = "use_sqlite")]
+pub fn litecli_client(sqlite_config_options: &commands::common::SqliteConfigOptions) {
+    let mut cmd = Command::new("litecli");
+    if let Some(filename) =  &sqlite_config_options.filename {
+        cmd.arg(filename);
+    }
+
+    cmd
+        .status()
+        .expect(&format!("failed to execute litecli ({:?})", cmd));
+}
+
+
+#[cfg(feature = "use_sqlite")]
+pub fn sqlite_client(sqlite_config_options: &commands::common::SqliteConfigOptions) {
+    let mut cmd = Command::new("sqlite3");
+    if let Some(filename) =  &sqlite_config_options.filename {
+        cmd.arg(filename);
+    }
+
+    cmd
+        .status()
+        .expect(&format!("failed to execute sqlite3 ({:?})", cmd));
+}
+
+
+#[cfg(feature = "use_sqlite")]
+pub fn sqlite_python_client(sqlite_config_options: &commands::common::SqliteConfigOptions) {
+    config::ensure_config_directory_exists();
+    let python_venv_dir = config::get_config_directory().join("python_venv");
+    if !python_venv_dir.exists() {
+        std::fs::create_dir(&python_venv_dir).unwrap();
+    }
+
+    let python_sqlite_venv = python_venv_dir.join("sqlite");
+    
+    if !python_sqlite_venv.exists() {
+        create_python_virtualenv(&python_sqlite_venv);
+        Command::new(python_sqlite_venv.join("bin").join("pip"))
+            .arg("install")
+            .arg("ipython")
+            .status()
+            .expect("could not install dependencies via pip");
+    }
+    let python_file = python_sqlite_venv.join("run.py");
+    if !python_file.exists() {
+        let content = include_str!("sqlite.py");
+        std::fs::File::create(&python_file).unwrap().write_all(content.as_ref()).unwrap();
+    }
+
+    if let Some(filename) =  &sqlite_config_options.filename {
+        std::env::set_var("SQLITE_FILE", filename);
+    }
+
+    Command::new(python_sqlite_venv.join("bin").join("python"))
         .arg(python_file.clone())
         .status()
         .expect(&format!("could not run python script: {}", python_file.to_str().unwrap()));
@@ -131,15 +197,25 @@ pub fn shell (_args: &ApplicationArguments, shell_command: &ShellCommand) {
         SourceConfigCommand::Mysql(mysql_config_options) => 
             match shell_command.client.as_ref() {
                 "mycli" => mycli_client(&mysql_config_options),
-                "mysql" => mysql_client(&mysql_config_options),
+                "default" | "mysql" => mysql_client(&mysql_config_options),
                 "python" => mysql_python_client(&mysql_config_options),
                 _ =>  {
                     eprintln!("unknown client: {}", shell_command.client);
                     std::process::exit(1);
                 }
             }
+
         #[cfg(feature = "use_sqlite")]
         SourceConfigCommand::Sqlite(sqlite_config_options) => {
+            match shell_command.client.as_ref() {
+                "litecli" => litecli_client(&sqlite_config_options),
+                "default" | "sqlite" => sqlite_client(&sqlite_config_options),
+                "python" => sqlite_python_client(&sqlite_config_options),
+                _ =>  {
+                    eprintln!("unknown client: {}", shell_command.client);
+                    std::process::exit(1);
+                }
+            }
         },
         #[cfg(feature = "use_postgres")]
         SourceConfigCommand::Postgres(postgres_config_options) => {
---
+++ src/commands/shell/sqlite.py
@@ -0,0 +1,32 @@
+import os
+from textwrap import dedent
+
+import sqlite3
+
+
+def get_conn():
+    return sqlite3.connect(os.environ.get('SQLITE_FILE', ':memory:'))
+
+
+def main():
+    msg = dedent("""
+    Variables and functions:
+        conn: database connection
+        cursor: connection cursor
+        get_conn(): obtain database connection
+        msg: function printing this message
+    """)
+    print(msg)
+    import IPython
+    conn = get_conn()
+    IPython.start_ipython(argv=[], user_ns={
+        'conn': conn,
+        'cursor': conn.cursor(),
+        'get_conn': get_conn,
+        'msg': lambda: print(msg)
+    })
+    raise SystemExit
+
+
+if __name__ == '__main__':
+    main()
