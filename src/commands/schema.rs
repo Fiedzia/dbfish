119f4c6a875286bc6caa3f30b8f24b485edd8f74
Maciej <fiedzia@gmail.com>
2019-09-04 03:24:14 +0100

    allow to search schema items

--- src/commands/schema.rs
+++ src/commands/schema.rs
@@ -1,5 +1,10 @@
+use std::collections::{HashMap, HashSet};
+
+use id_tree::{InsertBehavior, Node, NodeId, Tree};
+
 #[cfg(feature = "use_mysql")]
 use mysql;
+use regex::RegexBuilder;
 
 use crate::commands::{ApplicationArguments};
 use crate::commands::common::{SourceConfigCommandWrapper, SourceConfigCommand};
@@ -15,57 +20,105 @@ use crate::sources::sqlite::establish_sqlite_connection;
 
 #[derive(StructOpt)]
 pub struct SchemaCommand {
+    #[structopt(short = "r", long = "regex", help = "use regular expression engine")]
+    pub regex: bool,
+    #[structopt(short = "q", long = "query", help = "show items matching query")]
     pub query: Option<String>,
     #[structopt(subcommand)]
     pub source: SourceConfigCommandWrapper,
 }
 
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct DBItem {
     name: String,
-    items: Vec<DBItem>
 }
 
 impl DBItem {
     pub fn print(&self, indentation_level: usize) {
-        println!("{:indent$}{name}", "", indent=indentation_level, name=self.name);
-        for item in self.items.iter() {
-            item.print(indentation_level+4);
+        println!("{:indent$}{name}", "", indent=indentation_level * 4, name=self.name);
+    }
+
+    pub fn matches(&self, query: &str, is_regex: bool) -> bool {
+        if is_regex {
+            let re = RegexBuilder::new(query).case_insensitive(true).build().unwrap();
+            re.is_match(&self.name)
+        } else {
+            self.name.to_lowercase().contains(query)
         }
-
-    }
-
-    pub fn matches(&self, query: &str) -> bool {
-        return self.name.contains(query)
-    }
-
-    pub fn subtree_matching_query(&self, query: &str) -> Option<DBItem> {
-       None 
     }
 
 }
 
-#[derive(Clone)]
-struct DBItems (Vec<DBItem>);
-
+#[derive(Clone, Debug)]
+struct DBItems(Tree<DBItem>);
 
 impl DBItems {
-    pub fn print(&self) {
-       for dbitem in self.0.iter() {
-           dbitem.print(0)
-       } 
+
+    pub fn new() -> DBItems {
+        DBItems(Tree::new())
     }
 
-    pub fn subtree_matching_query(&self, query: &str) -> DBItems {
-        let mut dbitems = DBItems(vec![]);
-        for dbitem in self.0.iter() {
-            match dbitem.subtree_matching_query(query) {
-                Some(item) => dbitems.0.push(item),
-                None => {}
+    pub fn print(&self) {
+        match self.0.root_node_id() {
+            None => {},
+            Some(root_node_id) => {
+                for node_id in self.0.traverse_pre_order_ids(&root_node_id).unwrap() {
+                    let node = self.0.get(&node_id).unwrap();
+                    if !node.parent().is_none() {
+                        let indentation_level = self.0.ancestors(&node_id).unwrap().count() - 1;
+                        node.data().print(indentation_level);
+                    }
+                }
             }
-        };
-        dbitems
+        }
+    }
+
+    pub fn subtree_matching_query(&self, query: &str, is_regex:bool) -> DBItems {
+        match self.0.root_node_id() {
+            None => DBItems::new(),
+            Some(root_node_id) => {
+                let mut new_dbitems = DBItems::new();
+                let mut node_map = HashMap::new();
+                for node_id in self.0.traverse_post_order_ids(&root_node_id).unwrap() {
+                    if self.0.get(&node_id).unwrap().data().matches(query, is_regex) {
+                        let mut ancestor_ids:Vec<NodeId> = self.0
+                            .ancestor_ids(&node_id)
+                            .unwrap()
+                            .map(|v| v.clone())
+                            .collect();
+                        ancestor_ids.reverse();
+                        for node_id in ancestor_ids {
+                            let node = self.0.get(&node_id).unwrap();
+                            if node_map.contains_key(&node_id) {
+                                continue
+                            };
+                            let new_node_id = new_dbitems.0.insert(
+                                Node::new(node.data().clone()),
+                                match node.parent() {
+                                    None => InsertBehavior::AsRoot,
+                                    Some(parent_id) => InsertBehavior::UnderNode(node_map.get(parent_id).unwrap())
+                                }
+                            ).unwrap();
+                            node_map.insert(node_id, new_node_id);
+                        }
+                        if !node_map.contains_key(&node_id) {
+
+                            let node = self.0.get(&node_id).unwrap();
+                            let new_node_id = new_dbitems.0.insert(
+                                Node::new(node.data().clone()),
+                                match node.parent() {
+                                    None => InsertBehavior::AsRoot,
+                                    Some(parent_id) => InsertBehavior::UnderNode(node_map.get(parent_id).unwrap())
+                                }
+                            ).unwrap();
+                            node_map.insert(node_id, new_node_id);
+                        }
+                    }
+                }
+                new_dbitems
+            }
+        }
     }
 }
 
@@ -109,7 +162,7 @@ pub fn schema (_args: &ApplicationArguments, schema_command: &SchemaCommand) {
                     std::process::exit(1);
                 }
             };
-            let mut dbitems = DBItems(vec![]);
+            /*let mut dbitems = DBItems(vec![]);
             for row in results {
                 let (schema_name, table_name, column_name, column_type):(String, String, String, String) = mysql::from_row(row.unwrap());
                 if dbitems.0.is_empty() {
@@ -124,11 +177,19 @@ pub fn schema (_args: &ApplicationArguments, schema_command: &SchemaCommand) {
             if let Some(q) = &schema_command.query {
                 dbitems = dbitems.subtree_matching_query(&q);
             }
-            dbitems.print();
+            dbitems.print();*/
         },
         #[cfg(feature = "use_sqlite")]
         SourceConfigCommand::Sqlite(sqlite_config_options) => {
             let conn = establish_sqlite_connection(sqlite_config_options);
+            let mut dbitems = DBItems::new();
+            let root_node = dbitems.0.insert(
+                Node::new(
+                    DBItem{name: "".to_string()}
+                ),
+                InsertBehavior::AsRoot
+            ).unwrap();
+            let mut current_parent = None;
             conn.iterate("
                 SELECT 
                   m.name as table_name, 
@@ -146,17 +207,46 @@ pub fn schema (_args: &ApplicationArguments, schema_command: &SchemaCommand) {
                   m.name, 
                   p.cid
                 ",
-                |pairs| {
-                    for &(column, value) in pairs.iter() {
-                        println!("{}: {}", column, value.unwrap_or("NULL"))
+                |row| {
+                    let table_name = row[0].1.unwrap();
+                    let field_name = row[1].1.unwrap();
+                    match &current_parent {
+                        None => {
+                            current_parent = Some(
+                                dbitems.0.insert(
+                                    Node::new(
+                                        DBItem{name: table_name.to_string()}
+                                    ),
+                                    InsertBehavior::UnderNode(&root_node)
+                                ).unwrap()
+                            );
+                        },
+                        Some(node_id) => {
+                            if table_name != dbitems.0.get(node_id).unwrap().data().name {
+                                current_parent = Some(
+                                    dbitems.0.insert(
+                                        Node::new(
+                                            DBItem{name: table_name.to_string()}
+                                        ),
+                                        InsertBehavior::UnderNode(&root_node)
+                                    ).unwrap()
+                                );
+                            }
+                        }
                     }
+                    dbitems.0.insert(
+                        Node::new(
+                            DBItem{name: field_name.to_string()}
+                        ),
+                        InsertBehavior::UnderNode(current_parent.as_ref().unwrap())
+                    ).unwrap();
                     true
-                
                 }
-                ).unwrap();
-
-
-            
+            ).unwrap();
+            if let Some(query) = &schema_command.query {
+                dbitems = dbitems.subtree_matching_query(&query.to_lowercase(), schema_command.regex);
+            }
+            dbitems.print();
         },
         #[cfg(feature = "use_postgres")]
         SourceConfigCommand::Postgres(postgres_config_options) => {
